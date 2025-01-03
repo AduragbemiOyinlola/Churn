fc551a8ac3f3ae0d1ca6d7c5fe25a090acf39201
Aduragbemi <aduragbemioyinlola@gmail.com>
2025-01-03 22:38:09 +0100

    Convert inputs to model format

--- main.py
+++ main.py
@@ -26,3 +26,21 @@ paymentmethod = st.radio("Payment method", ('Bank transfer (automatic)', 'Credit
 monthlycharges = st.number_input("Monthly charges", min_value=0, max_value=200, value=0)
 totalcharges = st.number_input("Total charges", min_value=0, max_value=10000, value=0)
 
+# Convert inputs to model format
+gender = 1 if gender == 'Male' else 0
+seniorcitizen = 1 if seniorcitizen == 'Yes' else 0
+partner = 1 if partner == 'Yes' else 0
+dependents = 1 if dependents == 'Yes' else 0
+phoneservice = 1 if phoneservice == 'Yes' else 0
+multiplelines = 1 if multiplelines == 'Yes' else 0
+internetservice = 1 if internetservice == 'Fiber optic' else 0
+onlinesecurity = 1 if onlinesecurity == 'Yes' else 0
+onlinebackup = 1 if onlinebackup == 'Yes' else 0
+deviceprotection = 1 if deviceprotection == 'Yes' else 0
+techsupport = 1 if techsupport == 'Yes' else 0
+streamingtv = 1 if streamingtv == 'Yes' else 0
+streamingmovies = 1 if streamingmovies == 'Yes' else 0
+contract = 2 if contract == 'Two year' else 1 if contract == 'One year' else 0
+paperlessbilling = 1 if paperlessbilling == 'Yes' else 0
+paymentmethod = 3 if paymentmethod == 'Mailed check' else 2 if paymentmethod == 'Electronic check' else 1 if paymentmethod == 'Credit card (automatic)' else 0
+
